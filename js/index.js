678ca5cd46b938a447c631a4615deb0fdba73332
Zulfikar <fikar27@gmail.com>
2023-06-21 20:53:37 +0800

    done

--- js/index.js
+++ js/index.js
@@ -1,19 +1,55 @@
-function tombol(){
-    let inputSuhu = document.getElementById("input_angka").value
-    let output = document
+let suhuAsal = document.querySelector(".intext");
+let suhuHasil = document.querySelector(".outtext");
+let cara = document.querySelector(".kolomnya");
+let rumusSekarang = "c-f";
 
+function celkefah(){
+    if(rumusSekarang == "c-f"){
+        let x = Number(suhuAsal.value);
+        if(isNaN(x)){
+            suhuAsal.value = "";
+            return;
+        }
 
+        let y = x * 9/5 + 32;
 
+        suhuHasil.value = y;
+        cara.innerHTML = `${x} &deg;C x 9/5 + 32 = ${y} &deg;F`
+    }
+    if(rumusSekarang == "f-c"){
+        let x = Number(suhuAsal.value);
+        if(isNaN(x)){
+            suhuAsal.value = "";
+            return;
+        }
+        let y = (x-32)* 5/9;
 
+        suhuHasil.value = y;
+        cara.innerHTML = `(${x} &deg;F - 32) x 5/9 = ${y} &deg;C`
+    }
+}
 
+function reverse(){
+    if(rumusSekarang == "c-f"){
+        rumusSekarang = "f-c";
+        suhuAsal.value = suhuHasil.value 
+        celkefah();
+    }else{
+        rumusSekarang = "c-f";
+        suhuAsal.value = suhuHasil.value 
+        celkefah()
+    }
+}
 
+function reset(){
+    suhuAsal.value = "";
+    suhuHasil.value = "";
+    cara.value = "";
+}
 
-function tombol(){
-    let pertama = document.getElementById("angka-pertama").value
-    let kedua = document.getElementById("angka-kedua").value;
-    
-    let hasil = parseInt (pertama) + parseInt (kedua);
-    
-    let div = document.getElementById("hasil")
-
-    div.innerHTML = "hasilnya adalah " + hasil;
+suhuAsal.addEventListener("keypress",(key)=>{
+    if(key.charCode == "13"){
+        key.preventDefault();
+        celkefah();
+    }
+})
